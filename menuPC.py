54304a069feaa72e43c2b03ecf74d56f14f5dfac
Kristian Rose <62854656+kristianrose@users.noreply.github.com>
2022-11-23 18:19:58 +0000

    Update menuPC.py

--- menuPC.py
+++ menuPC.py
@@ -180,13 +180,13 @@ MatchCase_Function={
 def menu() -> None:
 	while Exit==False:
 		clear(clean)
-		option=str(input('''%s%s%s
-Bem-Vindo(a) ao %sXanax Buscas | Basico%s
+   	option=str(input('''%s%s%s
+Bem-Vindo(a) ao %sKinyPainel|Free%s
 Seu Endereço de IP : %s%s%s
 
-%s[%s Bitcoin : %s1Ff4bHfnpaZjjFvii1AkTjJgusN476paVA %s]%s
+%s[%s PIX : %s06acdbe2-ccf2-4c14-b8c1-7f0c965f629d %s]
 
-[Discord : %sxanax#9012 e ceu#0006%s]
+[Telegram : %s@K_iny%s | WhatsApp : %s+55 21 7918-0533%s]
 ____________________________
 |{ %s1%s } Consulta de DDD      |
 |{ %s2%s } Consulta de CNPJ     |
